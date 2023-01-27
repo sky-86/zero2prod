fe0360098a811f8e8ab8bc65ef9e6c872abd4e5f
sky-86 <sky86one@gmail.com>
2023-01-27 16:50:57 -0600

    added logging, in progess of migrating from log crate to tracing crate

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
+use env_logger::Env;
 use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::{config::get_config, startup::run};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     let config = get_config().expect("Failed to read config file");
 
     let connection_pool = PgPool::connect(&config.database.connection_string())
